de6cc55c67586513e03fd6c6e2bd86551a91a108
Lucas Oliveira <luksrn@gmail.com>
2013-04-13 12:31:03 -0300

    Atualização do blank project!

--- src/main/java/org/bootstrap/project/conf/AppConfig.java
+++ src/main/java/org/bootstrap/project/conf/AppConfig.java
@@ -6,13 +6,13 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 
 import com.github.javarch.jsf.config.JavaServerFacesConfig;
-import com.github.javarch.persistence.orm.hibernate.conf.H2DataSourceConfig;
+import com.github.javarch.persistence.orm.hibernate.conf.DataSourceH2Config;
 import com.github.javarch.persistence.orm.hibernate.conf.HibernateConfig;
 
 @Configuration
 @ComponentScan( basePackages = {"org.bootstrap"},
 		excludeFilters= { @ComponentScan.Filter( Configuration.class ) } )
-@Import({H2DataSourceConfig.class,
+@Import({DataSourceH2Config.class,
 		HibernateConfig.class,
 		JavaServerFacesConfig.class})
 @ImportResource({ "classpath*:/applicationContext-security.xml"})
